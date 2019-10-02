0d39ccbba1da2f3ef6d183878512c1b5ba7d6a1e
Christian Rodriguez <crodriguez@softeng.es>
2019-10-02 16:21:53 +0200

    generados

--- AspNetReactFacil/Scripts/JestTests/HelloWorldJest.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="../../node_modules/@types/jest/index.d.ts" />
-it("Should work", function () {
-    expect(true).toBe(true);
-});
-//# sourceMappingURL=HelloWorldJest.js.map
--- AspNetReactFacil/Scripts/Shared/SharedModule.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sharedFunction() {
-    alert('puto');
-}
-exports.sharedFunction = sharedFunction;
-//# sourceMappingURL=SharedModule.js.map
